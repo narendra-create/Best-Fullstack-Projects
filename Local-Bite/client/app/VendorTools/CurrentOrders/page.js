ae069582d574fe69adb1431678d6711e43ab04b1
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-10-29 12:06:51 +0530

    UPDATE: Ui Changes in CurrentOrders page

--- Local-Bite/client/app/VendorTools/CurrentOrders/page.js
+++ Local-Bite/client/app/VendorTools/CurrentOrders/page.js
@@ -2,12 +2,22 @@ import React from 'react'
 
 const Orders = () => {
     return (
-        <section className='w-full mt-54 bg-black h-full'>
+        <section className='w-full mt-34 bg-black h-full flex flex-col gap-10'>
             <div>
-                <h1>Your Current Orders</h1>
-                <p>You can update the status or cancel the order here</p>
+                <h1 className='text-4xl font-bold pl-6'>Your Orders:</h1>
+                <p className='text-sm font-extralight pl-6'>(You can update the status or cancel the order here)</p>
             </div>
-            <div>all orders from db here</div>
+            <div className='flex flex-col'>
+                <div className='pl-10 '>
+                    <h2 className='text-2xl font-semibold mb-5'>New Orders:</h2>
+                    <div className='h-full mx-10 bg-gray-700'>Db data</div>
+                </div>
+                <hr className='mx-5 rounded-full border-2 mt-10' />
+                <div className='pl-10 mt-5'>
+                    <h2 className='text-2xl font-semibold mb-5'>Current Orders:</h2>
+                    <div className='h-full mx-10 bg-gray-700'>Current orders db data</div>
+                </div>
+            </div>
         </section>
     )
 }
--- Local-Bite/client/app/VendorTools/Dashboard/page.js
+++ Local-Bite/client/app/VendorTools/Dashboard/page.js
@@ -4,18 +4,18 @@ import { useState } from 'react'
 
 const DashBoard = () => {
   //hooks
-  const [showDrawer, setShowDrawer] = useState(false)
+  //here
 
   return (
     <div className='text-black mt-34 mx-auto w-full flex items-center justify-center'>
       <section>
         <h1 className='font-bold text-3xl my-3'>Dashboard</h1>
-        {/* <ol>
+        <ol>
           <li>- History</li>
           <li>- Current orders + status update button</li>
           <li>- Add products button</li>
           <li>- Edit My products or delete them</li>
-        </ol> */}
+        </ol>
       </section>
     </div>
   )
