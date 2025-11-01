a4e117ed766de80f2455ebc52b4fd794de2244a1
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-11-01 15:18:38 +0530

    UPDATE: Converted hardcoded colors of ordercard to theme based colors

--- Local-Bite/client/app/Cards/CurrentCard/page.js
+++ Local-Bite/client/app/Cards/CurrentCard/page.js
@@ -1,8 +1,37 @@
 import React from 'react'
 
-const CurrentProducts = () => {
+const VendorProductsCard = ({ theme }) => {
+
+    const themeStyles = {
+        amber: {
+            border: 'border-amber-500',
+            bg: 'bg-amber-500',
+            text: 'text-amber-500',
+            hoverBg: 'hover:bg-amber-500',
+        },
+        blue: {
+            border: 'border-blue-400',
+            bg: 'bg-blue-400',
+            text: 'text-blue-400',
+            hoverBg: 'hover:bg-blue-400',
+        },
+        green: {
+            border: 'border-coriander-green',
+            bg: 'bg-coriander-green',
+            text: 'text-coriander-green',
+            hoverBg: 'hover:bg-coriander-green',
+        },
+        red: {
+            border: 'border-chili-red',
+            bg: 'bg-chili-red',
+            text: 'text-chili-red',
+            hoverBg: 'hover:bg-chili-red',
+        },
+    };
+    const color = themeStyles[theme] || 'blue-400';
+
     return (
-        <div className="relative mx-auto w-113 rounded-3xl border-1 border-blue-400 bg-blue-400 p-[0.8rem]">
+        <div className={`relative mx-auto w-113 rounded-3xl border-1 ${color.border} ${color.bg} p-[0.8rem]`}>
             <div className="bg-white rounded-[1rem] text-black p-5">
                 <div className='w-[60%] mx-2'>
                     <div className='flex flex-col mt-1'>
@@ -13,10 +42,10 @@ const CurrentProducts = () => {
                 </div>
                 <div className='absolute top-8 right-7 font-medium font-mono'>Preparing</div>
                 <div className='w-full justify-between px-2 items-center flex mt-8'>
-                    <button className='border-blue-400 border-3 rounded-2xl hover:text-white hover:bg-blue-400 transition-all ease-in-out duration-200 text-blue-400 px-4 text-xl font-normal py-2'>
+                    <button className={`${color.border} border-3 rounded-2xl hover:text-white ${color.hoverBg} transition-all ease-in-out duration-200 ${color.text} px-4 text-xl font-normal py-2`}>
                         Update Status
                     </button>
-                    <button className='border-blue-400 border-3 rounded-2xl hover:text-white hover:bg-blue-400 transition-all ease-in-out duration-200 text-blue-400 px-4 text-xl font-normal py-2'>
+                    <button className={`${color.border} border-3 rounded-2xl hover:text-white ${color.hoverBg} transition-all ease-in-out duration-200 ${color.text} px-4 text-xl font-normal py-2`}>
                         Ready ?
                     </button>
                 </div>
@@ -26,4 +55,4 @@ const CurrentProducts = () => {
     )
 }
 
-export default CurrentProducts
+export default VendorProductsCard;
--- Local-Bite/client/app/VendorTools/CurrentOrders/page.js
+++ Local-Bite/client/app/VendorTools/CurrentOrders/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import CurrentProducts from '@/app/Cards/CurrentCard/page'
+import VendorProductsCard from '@/app/Cards/CurrentCard/page'
 
 const Orders = () => {
     const testobj = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -12,14 +12,18 @@ const Orders = () => {
             <div className='flex flex-col'>
                 <div className='pl-10 '>
                     <h2 className='text-4xl font-semibold mb-5'>New Orders:</h2>
-                    <div className='h-full mx-10 bg-gray-700'>Db data</div>
+                    <div className='h-full mx-auto w-[78%] bg-gray-300 grid gap-8 grid-cols-3 items-center justify-center'>
+                        {testobj.map((OneOrder) => {
+                            return <VendorProductsCard theme={'amber'} key={OneOrder} />
+                        })}
+                    </div>
                 </div>
                 <hr className='mx-5 rounded-full border-2 mt-10' />
                 <div className='pl-10 mt-5 mx-auto w-full'>
                     <h2 className='text-4xl font-semibold mb-8 mx-auto w-417'>Current Orders:</h2>
                     <div className='h-full mx-auto w-[78%] bg-gray-300 grid gap-8 grid-cols-3 items-center justify-center'>
                         {testobj.map((OneOrder) => {
-                            return <CurrentProducts key={OneOrder} />
+                            return <VendorProductsCard theme={'blue'} key={OneOrder} />
                         })}
                     </div>
                 </div>
