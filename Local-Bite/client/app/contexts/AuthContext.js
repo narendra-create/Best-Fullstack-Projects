27e53ae77030c293a308bcaed3eed005ce620d85
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-11-10 13:50:35 +0530

    NEW: Added Context provider for accessing user in all components without many fetch requests

---
+++ Local-Bite/client/app/contexts/AuthContext.js
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+
+const Authcontext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+    const [User, setUser] = useState(null)
+    const [isLoading, setisLoading] = useState(true)
+
+    useEffect(() => {
+        const verifyUser = async () => {
+            try {
+                const res = await fetch(`${process.env.NEXT_PUBLIC_BACKENDURL}/api/verify`, { credentials: 'include' })
+                if (res.ok) {
+                    const data = await res.json();
+                    setUser(data.user)
+                }
+                else {
+                    console.log("Authentication failed server responded with", res.status)
+                    setUser(null)
+                }
+            }
+            catch (err) {
+                console.log("Error in authcontext")
+                setUser(null);
+            }
+            finally {
+                setisLoading(false)
+            }
+        }
+        verifyUser();
+    }, [])
+
+
+    return (
+        <Authcontext.Provider value={{ User, isLoading }}>
+            {children}
+        </Authcontext.Provider>
+    )
+}
+
+export const useAuth = () => useContext(Authcontext);
