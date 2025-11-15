f44d9fa55111c1b23caa48a61736eb4ea799600b
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-11-15 13:29:27 +0530

    NEW: Now working on status pages

---
+++ Local-Bite/client/app/Status/order-success/[orderid]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ordersuccess = ({ params }) => {
+    return (
+        <div className='text-black'>{params.orderid}</div>
+    )
+}
+
+export default ordersuccess
