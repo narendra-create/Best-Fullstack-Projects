dfcc1fbf8c0d0fd2ffc8ea4334cfd701518a1ad6
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-11-29 18:37:51 +0530

    FIX: Navbar buttons fixed  for logged in users and guests

--- Local-Bite/client/app/components/Navbar/page.js
+++ Local-Bite/client/app/components/Navbar/page.js
@@ -12,6 +12,9 @@ const Navbar = ({ toggleDrawer }) => {
     const isLoginpage = pathname === `/components/Login`;
     const isDashboard = pathname === '/VendorTools/Dashboard'
     const bgcolor = isRegisterpage || isLoginpage || pathname === '/' ? "white" : "black";
+    const isguest = !User;
+    const iscustomer = User && User.role === 'customer';
+    const isvendor = User && User.role === 'vendor';
     const dashboardcolor = isDashboard ? "border-black text-black hover:bg-black hover:text-white" : "text-white border-white  hover:bg-white hover:text-black"
 
     return (
@@ -35,7 +38,7 @@ const Navbar = ({ toggleDrawer }) => {
                             )}
                         </div>
                     )}
-                    {User && User.role === 'customer' ? <div className='flex md:gap-16 gap-3 items-center justify-center'>
+                    {(iscustomer || isguest) ? <div className='flex md:gap-16 gap-3 items-center justify-center'>
                         <button
                             onClick={toggleDrawer}
                             className="hidden md:block text-white bg-blue-700 px-5 py-2.5 rounded-lg"
@@ -51,7 +54,7 @@ const Navbar = ({ toggleDrawer }) => {
                         <Link href={'/components/Cart'}><svg className='cursor-pointer transition-all ease-in-out duration-200 hover:scale-108' xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill={bgcolor} viewBox="0 0 256 256"><path d="M238,82.73A8,8,0,0,0,232,80H187.63L134,18.73a8,8,0,0,0-12,0L68.37,80H24a8,8,0,0,0-7.93,9.06L31.14,202.12A16.06,16.06,0,0,0,47,216H209a16.06,16.06,0,0,0,15.86-13.88L239.93,89.06A8,8,0,0,0,238,82.73ZM81.6,184a7.32,7.32,0,0,1-.81,0,8,8,0,0,1-8-7.2l-5.6-56a8,8,0,0,1,15.92-1.6l5.6,56A8,8,0,0,1,81.6,184Zm54.4-8a8,8,0,0,1-16,0V120a8,8,0,0,1,16,0ZM89.63,80,128,36.15,166.37,80Zm99.13,40.8-5.6,56a8,8,0,0,1-7.95,7.2,7.32,7.32,0,0,1-.81,0,8,8,0,0,1-7.16-8.76l5.6-56a8,8,0,0,1,15.92,1.6Z"></path></svg>
                         </Link>
                     </div> : <div>
-                        {!isDashboard && <Link href={'/VendorTools/Dashboard'} className='font-semibold px-5 py-2 border-3 border-white text-white hover:bg-white hover:text-black rounded-2xl transition-all ease-in-out duration-200'>
+                        {!isDashboard && User && User.role === 'vendor' && <Link href={'/VendorTools/Dashboard'} className='font-semibold px-5 py-2 border-3 border-white text-white hover:bg-white hover:text-black rounded-2xl transition-all ease-in-out duration-200'>
                             DASHBOARD
                         </Link>}
                     </div>}
