5306fc1ea1251f3bec0bb51ed441c136065b28fd
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-11-30 19:27:17 +0530

    MINOR: Removing testing bg classes from divs

--- Local-Bite/client/app/VendorTools/CurrentOrders/page.js
+++ Local-Bite/client/app/VendorTools/CurrentOrders/page.js
@@ -82,8 +82,8 @@ const Orders = () => {
             </div>
             <div className='flex flex-col'>
                 <div className='md:pl-10'>
-                    <h2 className='text-3xl md:text-4xl font-semibold mb-2 md:w-417 md:mx-auto text-center bg-amber-50 py-3'>New Orders</h2>
-                    <div className='h-full md:mx-auto md:w-[78%] bg-gray-300 md:grid md:gap-y-0 md:gap-x-4 md:grid-cols-3 flex flex-col gap-5 items-center justify-center'>
+                    <h2 className='text-3xl md:text-4xl font-semibold mb-2 md:w-417 md:mx-auto text-center bg-gray-100 py-3'>New Orders</h2>
+                    <div className='h-full md:mx-auto md:w-[78%]  md:grid md:gap-y-0 md:gap-x-4 md:grid-cols-3 flex flex-col gap-5 items-center justify-center'>
                         {neworders && neworders.map((OneOrder) => {
                             return <VendorProductsCard theme={'amber'} key={OneOrder.orderid} Order={OneOrder} dbhandler={primaryhandler} />
                         })}
@@ -91,8 +91,8 @@ const Orders = () => {
                 </div>
                 <hr className='mx-5 rounded-full border-2 mt-10' />
                 <div className='md:pl-10 md:mt-5 mx-auto w-full'>
-                    <h2 className='text-3xl py-4 mx-5 mt-2 rounded-md bg-amber-50 md:text-4xl font-semibold mb-8 text-center md:text-left w-[90%] md:mx-auto md:w-417'>Current Orders</h2>
-                    <div className='h-full md:mx-auto md:w-[78%] bg-gray-300 md:grid md:gap-x-18 grid-cols-3 items-center flex flex-col gap-3 md:justify-center'>
+                    <h2 className='text-3xl py-4 mx-5 mt-2 rounded-md bg-gray-100 md:text-4xl font-semibold mb-8 text-center md:text-left w-[90%] md:mx-auto md:w-417'>Current Orders</h2>
+                    <div className='h-full md:mx-auto md:w-[78%] md:grid md:gap-x-18 grid-cols-3 items-center flex flex-col gap-3 md:justify-center'>
                         {currentorders && currentorders.map((OneOrder) => {
                             return <VendorProductsCard theme={'blue'} key={OneOrder.orderid} Order={OneOrder} dbhandler={primaryhandler} />
                         })}
