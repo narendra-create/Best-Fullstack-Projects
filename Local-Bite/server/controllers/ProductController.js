0575d770adafba2d3d7638236d3adeeb51355d35
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-12-07 22:57:11 +0530

    FEAT: Adding setstock and removeitem function in controller

--- Local-Bite/server/controllers/ProductController.js
+++ Local-Bite/server/controllers/ProductController.js
@@ -71,4 +71,54 @@ const getProductsbyUser = async (req, res) => {
     }
 }
 
+const setstock = async (req, res) => {
+    try {
+        const { user, role } = req.user;
+        const { isinstock, itemid } = req.body;
+        if (role !== "vendor") {
+            return res.status(401).json({ message: "Only Vendors Can do this" })
+        }
+
+        if (isinstock === undefined) {
+            return res.status(400).json({ message: "Please provide isinstock as true or false" })
+        }
+        if (typeof isinstock !== Boolean) {
+            return res.status(400).json({ message: "The isinstock should be boolean" })
+        }
+        const vendorfound = await Vendor.findOne({ user: user })
+        if (!vendorfound) {
+            return res.status(404).json({ message: "Vendor not found" })
+        }
+        const productfound = await Product.findOneAndUpdate({ vendor: vendorfound._id, _id: itemid }, { $set: { stock: isinstock } }, { new: true })
+        return res.status(200).json({ message: "Update success" })
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: "Server Error" })
+    }
+}
+
+const removeproduct = async (req, res) => {
+    try {
+        const { user, role } = req.user;
+        const { itemid } = req.params;
+        if (!itemid) {
+            return res.status(400).json({ message: "Please provide item id in url" })
+        }
+        if (role !== "vendor") {
+            return res.status(401).json({ message: "Only vendors can do this" })
+        }
+        const vendorfind = await Vendor.findOne({ user: user })
+        if (!vendorfind) {
+            return res.status(404).json({ message: "Vendor Not Found" })
+        }
+        const deleteproduct = await Product.findOneAndDelete({ vendor: vendorfind._id, _id: itemid })
+        return res.status(200).json({ message: "Item Deleted" })
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: "Server Error" })
+    }
+}
+
 export { getProductbyVendor, AddProduct, getAllProducts, getProductsbyUser };
