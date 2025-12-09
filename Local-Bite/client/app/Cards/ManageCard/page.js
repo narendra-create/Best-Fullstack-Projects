3d825ed71aad00fdf649d71889402d5360b5bab3
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-12-09 18:04:26 +0530

    UPDATE: Implementing logic in buttons

--- Local-Bite/client/app/Cards/ManageCard/page.js
+++ Local-Bite/client/app/Cards/ManageCard/page.js
@@ -1,6 +1,10 @@
+"use client"
 import React from 'react'
+import { useState } from 'react'
 
-const ManageItems = ({ product }) => {
+const ManageItems = ({ product, handlestock, handleremove }) => {
+    const [stock, setstock] = useState(product.stock)
+
     return (
         <div>
             <section className='relative shadow-[0_8px_30px_rgb(0,0,0,0.12)] w-200 bg-white text-black mx-auto rounded-2xl flex h-57 gap-4 transition-all ease-in-out duration-200 hover:scale-102'>
@@ -8,10 +12,16 @@ const ManageItems = ({ product }) => {
                 <div className='flex flex-col h-full justify-center'>
                     <div className='flex items-center justify-between h-18'>
                         <span className='text-3xl h-4 font-bold'>{product.name}</span>
-                        <button className='cursor-pointer absolute right-0 top-2.5 self-start bg-coriander-green px-4 py-2 mr-2 rounded-3xl text-white text-sm font-bold flex gap-1 items-center hover:bg-red-800 transition-all duration-200 focus:ring-2 focus:ring-black ease-in-out'>
-                            In Stock
+                        <button
+                            onClick={() => {
+                                const newval = !product.stock
+                                setstock(newval)
+                                handlestock(product._id, newval)
+                            }}
+                            className={`cursor-pointer absolute right-0 top-2.5 self-start ${stock ? "bg-coriander-green": "bg-chili-red"} px-4 py-2 mr-2 rounded-3xl text-white text-sm font-bold flex gap-1 items-center hover:bg-gray-800 transition-all duration-200 focus:ring-2 focus:ring-black ease-in-out`}>
+                            {stock ? "In Stock" : "Out of Stock"}
                         </button>
-                        <button className='cursor-pointer absolute right-0 top-14 self-start bg-chili-red px-4 py-2 mr-2 rounded-3xl text-white text-sm font-bold flex gap-1 items-center hover:bg-red-800 transition-all duration-200 focus:ring-2 focus:ring-black ease-in-out'>
+                        <button onClick={() => handleremove(product._id)} className='cursor-pointer absolute right-0 top-14 self-start bg-chili-red px-4 py-2 mr-2 rounded-3xl text-white text-sm font-bold flex gap-1 items-center hover:bg-red-800 transition-all duration-200 focus:ring-2 focus:ring-black ease-in-out'>
                             Remove
                         </button>
                     </div>
