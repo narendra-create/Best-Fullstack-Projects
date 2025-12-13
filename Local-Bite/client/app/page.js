f4e806393980de7f568dc4937afd8bfa63e1bdc3
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-12-13 15:08:03 +0530

    UI: Making page.js ui responsive

--- Local-Bite/client/app/page.js
+++ Local-Bite/client/app/page.js
@@ -2,8 +2,8 @@ import HomePage from "@/app/components/HomePage/page";
 
 export default function Home() {
   return (
-    <>
+    <div className="overflow-x-hidden">
       <HomePage />
-    </>
+    </div>
   );
 }
