7ee5904d21b4bc7560a98f5d682c1574da4ad93b
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-12-24 23:18:53 +0530

    UPDATE: Payment page moved from components to cards

---
+++ Local-Bite/client/app/Cards/Payment-page/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PaymentPage = () => {
+    return (
+            <section className='rounded-2xl my-[5%] w-full flex flex-col h-[90%] p-5 bg-gray-200 justify-center items-center'>
+                <div>total price to be paid - 500inr</div>
+                <div>Payment method</div>
+                <button>Pay and order</button>
+            </section>
+    )
+}
+
+export default PaymentPage
