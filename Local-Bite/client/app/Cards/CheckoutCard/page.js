44cddcead9b3e133c5e12aa258a403114dc8630c
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-12-28 22:39:36 +0530

    UPDATE: Adding rupee sign to prices

--- Local-Bite/client/app/Cards/CheckoutCard/page.js
+++ Local-Bite/client/app/Cards/CheckoutCard/page.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Checkout = ({ subTotal, discount, platformfee, deliverycharge, grandtotal, isCartEmpty, CheckoutClick }) => {
+const Checkout = ({ subTotal, discount, platformfee, deliverycharge, loading, grandtotal, isCartEmpty, methodclick }) => {
 
-    const [Methodpage, setMethodpage] = useState(false)
-   
+
+    if (loading) {
+        return <div className='loader w-full my-50 mx-auto'></div>
+    }
 
     return (
         <section className='bg-white rounded-xl'>
@@ -11,26 +13,26 @@ const Checkout = ({ subTotal, discount, platformfee, deliverycharge, grandtotal,
                 <div className='flex flex-col gap-2 mb-5 text-gray-600'>
                     <div className='flex justify-between mx-5 bg-amber-50 text-xl'>
                         <div className='font-semibold font-serif'>subtotal</div>
-                        <div>{subTotal}</div>
+                        <div>₹{subTotal}</div>
                     </div>
                     <div className='flex justify-between mx-5 bg-amber-50 text-xl'>
                         <div className='font-semibold font-serif'>delivery charge</div>
-                        <div>{subTotal > 0 ? deliverycharge : 0}</div>
+                        <div>₹{subTotal > 0 ? deliverycharge : 0}</div>
                     </div>
                     <div className='flex justify-between mx-5 bg-amber-50 text-xl'>
                         <div className='font-semibold font-serif'>platform fee</div>
-                        <div>{subTotal > 0 ? platformfee : 0}</div>
+                        <div>₹{subTotal > 0 ? platformfee : 0}</div>
                     </div>
                     <div className='flex justify-between mx-5 bg-amber-50 text-xl'>
                         <div className='font-semibold font-serif'>discount</div>
-                        <div>{subTotal > 0 ? discount : 0}</div>
+                        <div>₹{subTotal > 0 ? discount : 0}</div>
                     </div>
                 </div>
                 <div className='flex justify-between mx-5 text-2xl font-semibold bg-gray-100 mb-6'>
                     <div>Total -</div>
-                    <div>{subTotal > 0 ? grandtotal : 0}</div>
+                    <div>₹{subTotal > 0 ? grandtotal : 0}</div>
                 </div>
-                <button onClick={CheckoutClick} className={`rounded-2xl mx-[5%] py-3 hover:bg-lime-800 transition-all ease-in-out duration-150 bg-lime-600 font-bold w-88 text-white`} disabled={isCartEmpty || subTotal <= 0} >CheckOut</button>
+                <button onClick={methodclick} className={`rounded-2xl mx-[5%] py-3 hover:bg-lime-800 transition-all ease-in-out duration-150 bg-lime-600 font-bold w-88 text-white`} disabled={isCartEmpty || subTotal <= 0} >Continue</button>
             </div>
             <hr className='mx-5 mt-8 mb-3 border-t-4 border-gray-200' />
             <div className='md:mx-5 mt-5'>
