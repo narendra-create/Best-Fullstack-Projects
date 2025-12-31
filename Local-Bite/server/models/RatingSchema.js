d28439d7d083dd25871859b8a04f6434571b6002
narendra-create <176411268+narendra-create@users.noreply.github.com>
2025-12-31 13:44:17 +0530

    FEAT: Working on rating system, created Ratingschema to store ratings

---
+++ Local-Bite/server/models/RatingSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const Rating = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    vendor: { type: Schema.Types.ObjectId, ref: 'Vendor', required: true },
+    order: { type: Schema.Types.ObjectId, ref: 'Order', required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    review: { type: String },
+    createdAt: { type: Date, default: Date.now }
+})
+
+Rating.index({ user: 1, order: 1, unique: true })
+//user + order must be unique because only one review per order
+
+
+
+const RatingModel = mongoose.models.Rating || mongoose.model("Rating", Rating);
+export default RatingModel;
