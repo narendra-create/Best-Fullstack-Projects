8619b43951a2caf8200207d94e5ec65ca9c567f6
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-01-05 22:31:19 +0530

    FEAT: Added Route for Paying Online

--- Local-Bite/server/routes/OrderRoutes.js
+++ Local-Bite/server/routes/OrderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { placeOrder, updateorder, orderHistory, getcurrentorders, getsingleorder, cashorder } from '../controllers/OrderController.js'
+import { placeOrder, updateorder, payonline, orderHistory, getcurrentorders, getsingleorder, cashorder } from '../controllers/OrderController.js'
 import authMiddleware from '../middlewares/jwtcheck.js'
 
 const orderrouter = express.Router();
@@ -8,6 +8,7 @@ const orderrouter = express.Router();
 orderrouter.post('/place', authMiddleware, placeOrder);
 orderrouter.post('/place-cash', authMiddleware, cashorder);
 orderrouter.get('/current', authMiddleware, getcurrentorders);
+orderrouter.post('/pay-online/:orderid', authMiddleware, cashorder);
 orderrouter.patch('/updatestatus/:OrderId', authMiddleware, updateorder);
 orderrouter.get('/currentsingle/:orderid', authMiddleware, getsingleorder);
 orderrouter.get('/history', authMiddleware, orderHistory);
