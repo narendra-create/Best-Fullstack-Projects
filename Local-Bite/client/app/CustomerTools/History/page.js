d7c4c7b9d07045b0041b91987bb22297e74fa67b
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-01-09 16:18:54 +0530

    UPDATE: Adding fetch request for submitting review

--- Local-Bite/client/app/CustomerTools/History/page.js
+++ Local-Bite/client/app/CustomerTools/History/page.js
@@ -17,6 +17,36 @@ const OrderHistory = () => {
         setOrders(orders)
         console.log(orders)
     }
+    //selectedRating, tags, Review
+    const submitreview = async (selectedRating, orderid, tags, Review, vendor) => {
+        if (!vendor || !selectedRating || !orderid) {
+            alert("Please fill Required fields before submitting")
+            return;
+        }
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKENDURL}/api/review/submit`, {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                vendor: vendor,
+                stars: selectedRating,
+                review: Review,
+                orderid: orderid,
+                tags: tags
+            })
+        })
+
+        if (!res.ok) {
+            throw new Error("Error while fetching")
+        }
+        const { data } = await res.json();
+        console.log(`Review Added - ${data?.rating}`)
+        alert("Review Added")
+    }
+
     //use effects
     useEffect(() => {
         loadorders();
@@ -28,7 +58,7 @@ const OrderHistory = () => {
             <h1 className='text-2xl rounded-2xl font-bold text-center mt-2 mx-1 pr-6 pb-5 text-white pt-6 bg-chili-red'>My Orders</h1>
             <div className='mt-2 md:mx-auto flex flex-col md:grid md:grid-cols-3 md:items-start md:justify-center md:mt-8 md:w-[70%]'>
                 {Orders && Orders.map((order) => {
-                    return <OrderCard key={order.orderid} order={order} />
+                    return <OrderCard key={order._id} order={order} submitreview={submitreview} />
                 })}
             </div>
         </div>
