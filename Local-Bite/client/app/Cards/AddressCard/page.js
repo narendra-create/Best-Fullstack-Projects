e97dd1199c8c154330fc0f55b834705821166bbb
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-03-04 20:23:59 +0530

    UI: Making cards for addresses

--- Local-Bite/client/app/Cards/AddressCard/page.js
+++ Local-Bite/client/app/Cards/AddressCard/page.js
@@ -2,18 +2,20 @@ import React from 'react'
 
 const AddressCard = () => {
     return (
-        <section className='flex flex-col'>
-            <div>Label</div>
-            <div>
-                <div>Name</div>
-                <div>street and landmark</div>
-                <div>City, state,pincode,country</div>
-                <div>Phone no.</div>
+        <section className='bg-white shadow-[0px_0px_10px_-6px_rgba(0,0,0,0.35)] flex flex-col border border-gray-300 rounded-t-xl pt-5'>
+            <div className='flex flex-col px-5 pb-4'>
+                <div className='text-xl font-semibold mb-2'>Home</div>
+                <div>
+                    <div>Narendra Modi</div>
+                    <div>490225, Delhi, near india gate</div>
+                    <div>New Delhi,Delhi,India</div>
+                    <div>+91 9876543210</div>
+                </div>
             </div>
-            <div>
-                <button>Edit</button>
-                <button>Delete</button>
-                <button>Set Default</button>
+            <div className='h-12 w-full flex items-center bg-gray-200'>
+                <button className='w-[32%] cursor-pointer h-[65%] text-md font-sans text-blue-700'>Edit</button>
+                <button className='w-[32%] cursor-pointer h-[65%] text-md font-sans border-x border-gray-400 text-rose-700'>Delete</button>
+                <button className='w-[36%] cursor-pointer h-[65%] text-md font-sans '>Set Default</button>
             </div>
         </section>
     )
