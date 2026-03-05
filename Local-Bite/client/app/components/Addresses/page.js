bc79cdc0d1930d2a89732647e4204fa1db07d7d2
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-03-05 19:39:57 +0530

    UPDATE: Added Loader on page and useeffect to fetch the data

--- Local-Bite/client/app/components/Addresses/page.js
+++ Local-Bite/client/app/components/Addresses/page.js
@@ -11,6 +11,7 @@ const MyAddresses = () => {
     //states
     const [adding, setadding] = useState(false);
     const [Addresses, setAddresses] = useState();
+    const [pageloading, setpageloading] = useState(true);
     //Outside data
     const { User, isLoading } = useAuth();
     //functions
@@ -35,6 +36,7 @@ const MyAddresses = () => {
             else {
                 const { Addresses } = await res.json();
                 setAddresses(Addresses);
+                setpageloading(false);
             }
         }
         catch (err) {
@@ -87,8 +89,12 @@ const MyAddresses = () => {
         }
     }
     //useeffects
+
+    useEffect(() => {
+        fetchaddresses();
+    }, [])
     //Page code
-    if (isLoading) {
+    if (isLoading && pageloading) {
         return <div role="status" className='flex items-center justify-center w-40 mx-auto h-screen'>
             <svg aria-hidden="true" className="inline w-24 h-24 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
