7622b968af3b23851f9851e6874ae4e8c1d2eaa5
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-03-05 19:17:06 +0530

    UPDATE: Adding Address add popup in address page

--- Local-Bite/client/app/components/Addresses/page.js
+++ Local-Bite/client/app/components/Addresses/page.js
@@ -1,21 +1,30 @@
+"use client";
 import React from 'react';
+import { useState, useEffect } from 'react';
 import AddressCard from '@/app/Cards/AddressCard/page.js';
+import AddressPopup from '@/components/ui/AddressPopup/page.js';
 import { ArrowLeft, Plus } from 'lucide-react';
 
 const MyAddresses = () => {
+    const [adding, setadding] = useState(false);
+
+    const handlesubmitaddress = async (object) => {
+        console.log(object)
+    }
+
     const demo = [1, 2, 3, 4, 5]
     return (
         <div className='w-full min-h-screen bg-gray-100'>
             <div className='cursor-text shadow-[0px_0px_55px_-50px_#000000] w-full py-1 h-18 md:h-24 mt-22 flex items-center gap-14 md:gap-0 border-y border-gray-400 bg-white'>
                 <button className='pl-5 md:pl-12'>
-                    <ArrowLeft size={32} />
+                    <ArrowLeft />
                 </button>
-                <span className='text-center md:text-right pr-[26%] font-mono self-center w-[52%]  md:w-[82%] text-2xl md:text-4xl font-semibold'>Manage - Addresses</span>
-                <button className='hover:font-bold hover:scale-98 hover:text-shadow-md transition-all ease-in-out duration-150 hover:inset-shadow-[inset_0px_0px_12px_0px_rgba(0,0,0,0.2)] md:flex hidden gap-2 px-6 items-center justify-center py-3 rounded-lg text-lg bg-trust text-white'>
+                <span className='text-center md:text-right md:pr-[26%] font-mono self-center w-[52%] md:w-[82%] text-xl md:text-4xl font-semibold'>Manage-Addresses</span>
+                <button onClick={() => setadding(true)} className='hover:font-bold hover:scale-98 hover:text-shadow-md transition-all ease-in-out duration-150 hover:inset-shadow-[inset_0px_0px_12px_0px_rgba(0,0,0,0.2)] md:flex hidden gap-2 px-6 items-center justify-center py-3 rounded-lg text-lg bg-trust text-white'>
                     <span className='text-center align-middle'><Plus /></span><span className='font-semibold'>Add New Address</span>
                 </button>
             </div>
-            <button className='bg-white md:hidden shadow-[0px_0px_10px_-6px_rgba(0,0,0,0.35)] flex justify-center gap-8 w-[96%] mx-auto text-xl items-center h-14 rounded-lg border border-gray-400 my-2'>
+            <button onClick={() => setadding(true)} className='bg-white md:hidden shadow-[0px_0px_10px_-6px_rgba(0,0,0,0.35)] flex justify-center gap-8 w-[96%] mx-auto text-xl items-center h-14 rounded-lg border border-gray-400 my-2'>
                 <span className='text-center align-middle'><Plus /></span><span className='font-semibold'>Add New Address</span>
             </button>
             <div className='px-5 flex flex-col md:grid grid-cols-3 md:gap-4 md:px-16'>
@@ -25,6 +34,7 @@ const MyAddresses = () => {
                     </div>
                 })}
             </div>
+            <AddressPopup isOpen={adding} onClose={() => setadding(false)} onSubmit={handlesubmitaddress} />
         </div>
     )
 }
