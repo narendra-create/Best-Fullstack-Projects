5803960783611c2e709a493b43dd25f6865100b6
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-03-05 18:33:35 +0530

    UPDATE: Ui completed for big screens

--- Local-Bite/client/app/components/Addresses/page.js
+++ Local-Bite/client/app/components/Addresses/page.js
@@ -6,16 +6,19 @@ const MyAddresses = () => {
     const demo = [1, 2, 3, 4, 5]
     return (
         <div className='w-full min-h-screen bg-gray-100'>
-            <div className='cursor-text shadow-[0px_0px_55px_-50px_#000000] w-full py-1 h-18 mt-22 flex items-center gap-14 border-y border-gray-400 bg-white'>
-                <button className='pl-5'>
-                    <ArrowLeft />
+            <div className='cursor-text shadow-[0px_0px_55px_-50px_#000000] w-full py-1 h-18 md:h-24 mt-22 flex items-center gap-14 md:gap-0 border-y border-gray-400 bg-white'>
+                <button className='pl-5 md:pl-12'>
+                    <ArrowLeft size={32} />
                 </button>
-                <span className='text-center self-center w-[52%] text-2xl font-semibold'>Manage Addresses</span>
+                <span className='text-center md:text-right pr-[26%] font-mono self-center w-[52%]  md:w-[82%] text-2xl md:text-4xl font-semibold'>Manage - Addresses</span>
+                <button className='hover:font-bold hover:scale-98 hover:text-shadow-md transition-all ease-in-out duration-150 hover:inset-shadow-[inset_0px_0px_12px_0px_rgba(0,0,0,0.2)] md:flex hidden gap-2 px-6 items-center justify-center py-3 rounded-lg text-lg bg-trust text-white'>
+                    <span className='text-center align-middle'><Plus /></span><span className='font-semibold'>Add New Address</span>
+                </button>
             </div>
             <button className='bg-white md:hidden shadow-[0px_0px_10px_-6px_rgba(0,0,0,0.35)] flex justify-center gap-8 w-[96%] mx-auto text-xl items-center h-14 rounded-lg border border-gray-400 my-2'>
                 <span className='text-center align-middle'><Plus /></span><span className='font-semibold'>Add New Address</span>
             </button>
-            <div className='px-5'>
+            <div className='px-5 flex flex-col md:grid grid-cols-3 md:gap-4 md:px-16'>
                 {demo && demo.map((single) => {
                     return <div key={single} className='my-5'>
                         <AddressCard />
