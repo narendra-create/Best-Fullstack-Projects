f5b9c0fe8ce47b6d33bbff8a48706d5cd4dbd509
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-04-13 20:14:14 +0530

    FEAT: Now we can choose mode for each operation

--- Local-Bite/client/components/ui/AddressPopup/page.js
+++ Local-Bite/client/components/ui/AddressPopup/page.js
@@ -1,7 +1,7 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-export default function AddressPopup({ isOpen, onClose, onSubmit }) {
+export default function AddressPopup({ currentaddress, isOpen, onClose, onSubmit, mode }) {
     const [formdata, setformdata] = useState({
         label: "",
         street: "",
@@ -10,7 +10,18 @@ export default function AddressPopup({ isOpen, onClose, onSubmit }) {
         pincode: "",
         landmark: "",
         phone: ""
-    })
+    });
+
+    useEffect(() => {
+        if (mode === "updateaddress" && currentaddress) {
+            setformdata(currentaddress[0]);
+        }
+    }, [mode, currentaddress]);
+
+    // useEffect(() => {
+    //     console.log(formdata, "curentaddress", currentaddress)
+    // }, [formdata, currentaddress])
+
 
     if (!isOpen) return null;
 
@@ -23,11 +34,17 @@ export default function AddressPopup({ isOpen, onClose, onSubmit }) {
 
     const handlesubmit = (e) => {
         e.preventDefault();
-        onSubmit(formdata)
+        if (mode === "newaddress") {
+            onSubmit(formdata)
+        }
+        else if (mode === "updateaddress") {
+            onSubmit(formdata, currentaddress[0]._id)
+        }
         setTimeout(() => {
             onClose();
         }, 1000);
     }
+
 
     return (
         <div className="overlay">
@@ -37,16 +54,16 @@ export default function AddressPopup({ isOpen, onClose, onSubmit }) {
 
                 <form className="addressForm" onSubmit={(e) => handlesubmit(e)}>
 
-                    <input onChange={handlechange} name="label" placeholder="Label (Home, Office)" />
-                    <input onChange={handlechange} name="street" placeholder="Street Address" />
-                    <input onChange={handlechange} name="city" placeholder="City" />
-                    <input onChange={handlechange} name="state" placeholder="State" />
-                    <input onChange={handlechange} name="pincode" placeholder="Postal Code" />
-                    <input onChange={handlechange} name="landmark" placeholder="Landmark" />
-                    <input onChange={handlechange} name="phone" placeholder="Phone Number" />
+                    <input onChange={handlechange} name="label" value={formdata.label} placeholder="Label (Home, Office)" />
+                    <input onChange={handlechange} name="street" value={formdata.street} placeholder="Street Address" />
+                    <input onChange={handlechange} name="city" value={formdata.city} placeholder="City" />
+                    <input onChange={handlechange} name="state" value={formdata.state} placeholder="State" />
+                    <input onChange={handlechange} name="pincode" value={formdata.pincode} placeholder="Postal Code" />
+                    <input onChange={handlechange} name="landmark" value={formdata.landmark} placeholder="Landmark" />
+                    <input onChange={handlechange} name="phone" value={formdata.phone} placeholder="Phone Number" />
 
                     <div className="buttons">
-                        <button type="submit" className="bg-trust py-2 px-6 text-white font-semibold rounded-md">Save</button>
+                        <button type="submit" className="bg-trust py-2 px-6 text-white font-semibold rounded-md">{mode === "newaddress" ? "Save" : "Update"}</button>
                         <button type="button" className="bg-chili-red py-2 px-6 text-white font-semibold rounded-md" onClick={onClose}>
                             Cancel
                         </button>
