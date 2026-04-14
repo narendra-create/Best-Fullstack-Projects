b8305aa550c3a80010dc0c34ec2f555596e752d2
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-04-14 20:11:34 +0530

    UPDATE: Tweaking for address feature

--- Local-Bite/client/app/Cards/CheckoutCard/page.js
+++ Local-Bite/client/app/Cards/CheckoutCard/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Checkout = ({ subTotal, discount, platformfee, deliverycharge, loading, grandtotal, isCartEmpty, methodclick }) => {
+const Checkout = ({ subTotal, addressclick, discount, platformfee, deliverycharge, loading, grandtotal, isCartEmpty, methodclick }) => {
 
 
     if (loading) {
@@ -32,7 +32,7 @@ const Checkout = ({ subTotal, discount, platformfee, deliverycharge, loading, gr
                     <div>Total -</div>
                     <div>₹{subTotal > 0 ? grandtotal : 0}</div>
                 </div>
-                <button onClick={methodclick} className={`rounded-2xl mx-[5%] py-3 hover:bg-lime-800 transition-all ease-in-out duration-150 bg-lime-600 font-bold w-88 text-white`} disabled={isCartEmpty || subTotal <= 0} >Continue</button>
+                <button onClick={addressclick} className={`rounded-2xl mx-[5%] py-3 hover:bg-lime-800 transition-all ease-in-out duration-150 bg-lime-600 font-bold w-88 text-white`} disabled={isCartEmpty || subTotal <= 0} >Continue</button>
             </div>
             <hr className='mx-5 mt-8 mb-3 border-t-4 border-gray-200' />
             <div className='md:mx-5 mt-5'>
