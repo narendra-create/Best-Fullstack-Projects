84003f002b9f66957f5cf45477ff01774cda34dc
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-04-18 16:13:22 +0530

    FEAT: Schema for coupon feature

---
+++ Local-Bite/server/models/CouponSchema.js
@@ -0,0 +1,96 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const couponschema = new Schema(
+    {
+        code: {
+            type: String,
+            required: true,
+            unique: true,
+            uppercase: true,
+            trim: true,
+        },
+
+        discountType: {
+            type: String,
+            enum: ["percentage", "flat", "free_delivery"],
+            required: true,
+        },
+
+        discountValue: {
+            type: Number,
+            default: 0, // not needed for free_delivery
+        },
+
+        maxDiscountAmount: Number,
+
+        minOrderAmount: {
+            type: Number,
+            default: 0,
+        },
+
+        // Food delivery specific
+        applicableRestaurants: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Vendor",
+            },
+        ],
+        // empty = all restaurants
+
+        // user targeting
+        userType: {
+            type: String,
+            enum: ["all", "new", "existing"],
+            default: "all",
+        },
+
+        // usage control
+        usageLimit: Number,
+        usedCount: {
+            type: Number,
+            default: 0,
+        },
+
+        perUserLimit: {
+            type: Number,
+            default: 1,
+        },
+
+        usersUsed: [
+            {
+                userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+                usedAt: { type: Date, default: Date.now },
+            },
+        ],
+
+        // timing
+        startDate: {
+            type: Date,
+            required: true,
+        },
+
+        expiryDate: {
+            type: Date,
+            required: true,
+        },
+
+        validDays: {
+            type: [String], // ["Mon", "Tue"]
+        },
+
+        validTimeRange: {
+            start: String, // "10:00"
+            end: String,   // "14:00"
+        },
+
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const Coupons = mongoose.models.Couponschema | mongoose.model("Coupons", couponschema);
+export default Coupons;
