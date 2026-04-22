51c4964c040d80d0c28d4c56cdc1367e0d58ff57
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-04-22 15:28:07 +0530

    FIXED button issue part 2

--- Local-Bite/client/app/VendorTools/Coupons/page.js
+++ Local-Bite/client/app/VendorTools/Coupons/page.js
@@ -44,10 +44,10 @@ export default function CouponsPage() {
     }
 
     return (
-        <div className="p-6 bg-gray-50 w-[99vw] min-h-screen">
+        <div className="p-6 bg-gray-50 w-[99vw] md:w-full min-h-screen">
 
             {/* Header */}
-            <div className="flex flex-col md:flex-row gap-5 md:gap-0 md:justify-normal justify-between items-center mb-6">
+            <div className="flex flex-col md:flex-row gap-5 md:gap-5 justify-between items-center mb-6">
                 <div>
                     <h1 className="text-2xl md:text-4xl font-bold text-center">Coupons Management</h1>
                     <p className="text-xs md:text-lg text-gray-500">
