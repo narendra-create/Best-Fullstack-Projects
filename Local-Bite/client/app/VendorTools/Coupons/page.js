a9f7479ac16a2f4159bc880b0b1b6a11da3d6476
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-04-22 14:48:51 +0530

    UPDATE: Fetching from api

--- Local-Bite/client/app/VendorTools/Coupons/page.js
+++ Local-Bite/client/app/VendorTools/Coupons/page.js
@@ -1,59 +1,47 @@
 "use client"
 import CouponCard from "@/app/Cards/CouponCard/page";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function CouponsPage() {
+    const [Coupons, setCoupons] = useState();
+    const [Loading, setLoading] = useState(true);
 
-    const coupons = [
-        {
-            id: 1,
-            code: "Summer Special Discount",
-            description: "Celebrate summer with exclusive discounts on all menu items.",
-            discountValue: 25,
-            discountType: "percentage",
-            expiryDate: "Jul 31, 2026",
-        },
-        {
-            id: 2,
-            code: "New Customer Welcome",
-            description: "Flat discount for first-time customers.",
-            discountValue: 10,
-            discountType: "flat",
-            expiryDate: "Dec 31, 2026",
-        },
-        {
-            id: 3,
-            code: "Weekend Brunch Deal",
-            description: "Special weekend offer to attract more customers.",
-            discountValue: 15,
-            discountType: "percentage",
-            expiryDate: "Jun 30, 2026",
-        },
-        {
-            id: 4,
-            code: "Happy Hour Special",
-            description: "Boost evening sales between 4 PM and 7 PM.",
-            discountValue: 20,
-            discountType: "percentage",
-            expiryDate: "Aug 15, 2026",
-        },
-        {
-            id: 5,
-            code: "Loyalty Reward",
-            description: "Reward returning customers with a small discount.",
-            discountValue: 5,
-            discountType: "flat",
-            expiryDate: "May 1, 2026",
-        },
-        {
-            id: 6,
-            code: "Family Combo Saver",
-            description: "Perfect savings for groups of 4 or more.",
-            discountValue: 30,
-            discountType: "percentage",
-            expiryDate: "Sep 30, 2026",
-        },
-    ];
+    const fetchcoupons = async () => {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKENDURL}/api/coupon/getcoupons`);
+        const data = await res.json();
+        if (!res.ok) {
+            toast.error(`${data.message}`, {
+                position: "top-center",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Slide,
+            });
+            setLoading(false);
+            return;
+        }
+        setCoupons(data.coupons);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchcoupons();
+    }, [])
+
+    if (Loading) {
+        return <div role="status" className='flex items-center justify-center w-40 mx-auto h-screen'>
+            <svg aria-hidden="true" className="inline w-24 h-24 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+            </svg>
+            <span className="sr-only">Loading...</span>
+        </div>
+    }
 
     return (
         <div className="p-6 bg-gray-50 w-[99vw] min-h-screen">
@@ -75,9 +63,9 @@ export default function CouponsPage() {
 
             {/* Grid */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-[92vw] md:w-360 mx-auto md:mt-14">
-                {coupons.map((coupon) => (
+                {Coupons.map((coupon) => (
                     <CouponCard
-                        key={coupon.id}
+                        key={coupon._id}
                         coupon={coupon}
                     />
                 ))}
