cb5f5c6c37d46071c4050352328e0c5a07f99003
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-05-03 16:31:54 +0530

    NEW: Adding cloudinary for uploading images

---
+++ Local-Bite/server/config/cloudinary.js
@@ -0,0 +1,15 @@
+import { v2 as cloudinary } from "cloudinary";
+
+if (!process.env.CLOUDINARY_NAME ||
+    !process.env.CLOUDINARY_KEY ||
+    !process.env.CLOUDINARY_SECRET) {
+    throw new Error("Cloudinary config missing in environment variables");
+}
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
+
+export default cloudinary
