2767a7ed2d9d1b726fea969bc78afe8211b9eca5
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-05-05 14:17:32 +0530

    UPDATE: Added multer middleware to parse upcoming files

--- Local-Bite/server/routes/UserRoutes.js
+++ Local-Bite/server/routes/UserRoutes.js
@@ -1,11 +1,12 @@
 import { userregister, userlogin, userlogout, Addadress, removeaddress, getaddress, updateaddress, makedefault } from "../controllers/UserController.js";
 import authMiddleware from '../middlewares/jwtcheck.js'
 import express from 'express';
+import upload from "../config/multer.js";
 
 const userroutes = express.Router();
 
 //For register 
-userroutes.post('/', userregister);
+userroutes.post('/', upload.single("image"), userregister);
 //For login
 userroutes.post('/login', userlogin);
 //For Logout
